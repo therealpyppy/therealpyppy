8ed1e6da681a63d7004cfe8f4ab4156afaefd104
Iain Fox <132091707+therealpyppy@users.noreply.github.com>
2025-05-15 23:23:05 -0500

    Update update_countdown.py

--- update_countdown.py
+++ update_countdown.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 afterContent = "} days until my summer break! 🎉🎊\n</div>\n\n<div align=\"center\">\n  <img src=\"https://github-readme-stats.vercel.app/api?username=therealpyppy&theme=prussian&show_icons=true&hide_border=true&count_private=true\"/>\n</div>\n\n<div align=\"center\">\n  <img src=\"https://streak-stats.demolab.com?user=therealpyppy&theme=prussian&hide_border=true\"/>\n</div>\n\n<div align=\"center\">\n  <img src=\"https://github-readme-stats.vercel.app/api/top-langs/?username=therealpyppy&theme=prussian&show_icons=true&hide_border=true&layout=compact\"/>\n</div>"
 
 new_content = content.replace(
-    content.split("{")[1]+afterContent, str(days_left)
+    content.split("{")[1], str(days_left)+afterContent
 )
 
 with open("README.md", "w", encoding="utf-8") as f:
