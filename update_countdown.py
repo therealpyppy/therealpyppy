0de1d5ed58a4d20a4a011d1c91cb8e4322192b43
Iain Fox <132091707+therealpyppy@users.noreply.github.com>
2025-05-15 23:18:51 -0500

    Update update_countdown.py

--- update_countdown.py
+++ update_countdown.py
@@ -10,9 +10,10 @@ days_left = (target - today).days
 with open("README.md", "r", encoding="utf-8") as f:
     content = f.read()
 
+afterContent = "} days until my summer break! 🎉🎊\n</div>\n\n<div align="center">\n  <img src="https://github-readme-stats.vercel.app/api?username=therealpyppy&theme=prussian&show_icons=true&hide_border=true&count_private=true"/>\n</div>\n\n<div align="center">\n  <img src="https://streak-stats.demolab.com?user=therealpyppy&theme=prussian&hide_border=true"/>\n</div>\n\n<div align="center">\n  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=therealpyppy&theme=prussian&show_icons=true&hide_border=true&layout=compact"/>\n</div>"
+
 new_content = content.replace(
-    content.split("{")[1], str(days_left)
-)
+    content.split("{")[1]+afterContent, str(days_left)
 
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(new_content)
